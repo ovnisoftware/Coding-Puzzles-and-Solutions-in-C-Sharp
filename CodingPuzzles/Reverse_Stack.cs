72667f189b9fa000ba670e4bdb86571488910c0d
ovnisoftware <info@ovnisoftware.com>
2015-05-07 18:19:20 -0400

    Reverse Stack

---
+++ CodingPuzzles/Reverse_Stack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;  //Necessary for Stack
+
+namespace CodingPuzzles
+{
+    //Reverse a stack
+    public static class Reverse_Stack
+    {
+        public static Stack reverse(Stack input)
+        {
+            //Declare another stack to store the values from the passed stack
+            Stack temp = new Stack();
+
+            //While the passed stack isn't empty, pop elements from the passed stack onto the temp stack
+            while (input.Count != 0)
+                temp.Push(input.Pop());
+
+            return temp;
+        }
+    }
+}
