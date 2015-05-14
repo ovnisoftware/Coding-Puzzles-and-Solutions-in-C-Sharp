0f898a22bf89213a14d6cb831ed8a32674a98703
ovnisoftware <info@ovnisoftware.com>
2015-05-14 14:32:50 -0400

    Unique String

--- CodingPuzzles/Program.cs
+++ CodingPuzzles/Program.cs
@@ -11,7 +11,7 @@ namespace CodingPuzzles
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(Check_Prime.IsPrime(13));
         }
     }
 }
---
+++ CodingPuzzles/Unique_String.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    //Implement an algorithm to determine if a string has all unique characters.
+    //What if you cannot use additional data structures?
+    //Source: Cracking the Coding Interview p. 73
+
+    //Uses a dictionary to store each character and checks for a duplicate entry
+    public static class Unique_String
+    {
+        public static bool IsUnique(string s)
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (d.ContainsKey(c))
+                    return false;
+                else
+                    d.Add(c, 1);
+            }
+            return true;
+        }
+    }
+}
