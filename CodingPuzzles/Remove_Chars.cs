7ddc08087c8fb7c17521bc85a7727620bd551373
ovnisoftware <info@ovnisoftware.com>
2015-09-01 10:26:08 -0400

    Remove Chars

---
+++ CodingPuzzles/Remove_Chars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    //Remove all characters from a string except for these characters: [ { } ( ) ] without using Regex.
+    //Regex way:
+    //x = Regex.Replace(x, "[^{}\\][()]", "");
+    public static class Remove_Chars
+    {
+        public static string Remove(string x)
+        {
+            string result = "";
+            char[] arr = new char[] { '[', '{', '(', ')', '}', ']' };
+            foreach (char c in arr)
+            {
+                if (arr.Contains(c))
+                    result += c.ToString();
+            }
+            return result;
+        }
+        //string x = "(test)[1234]{testagain!}";
+        //Console.WriteLine(Remove_Chars.Remove(x)); 
+    }
+}
