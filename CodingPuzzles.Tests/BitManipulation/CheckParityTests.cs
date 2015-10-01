2597816e722cf354b81849e8a35f10eef1b20e3d
ovnisoftware <info@ovnisoftware.com>
2015-10-01 11:11:23 -0400

    CheckParity.cs and uploaded UnitTest Project
    Use bitwise operators to determine if an integer is even

---
+++ CodingPuzzles.Tests/BitManipulation/CheckParityTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.BitManipulation
+{
+    [TestClass]
+    public class CheckParityTests
+    {
+        [TestMethod]
+        public void IsEven_EvenNumberPassed_ReturnsTrue()
+        {
+            Assert.IsTrue(CheckParity.IsEven(2));
+        }
+        [TestMethod]
+        public void IsEven_OddNumberPassed_ReturnsFalse()
+        {
+            Assert.IsFalse(CheckParity.IsEven(7));
+        }
+        [TestMethod]
+        public void IsOdd_OddNumberPassed_ReturnsTrue()
+        {
+            Assert.IsTrue(CheckParity.IsOdd(7));
+        }
+        [TestMethod]
+        public void IsOdd_EvendNumberPassed_ReturnsFalse()
+        {
+            Assert.IsFalse(CheckParity.IsOdd(2));
+        }
+    }
+}
---
+++ CodingPuzzles/BitManipulation/CheckParity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    //Use bitwise operators to determine if an integer is even
+    //If an integer is odd, the farthest right bit will always be set to 1
+    //Ex. 7 (111)
+    //If an integer is even, the farthest right bit will always be set to 0
+    //Ex. 8 (1000)
+    //AND Truth Table
+    //A B Output
+    //0 0 0
+    //1 0 0
+    //0 1 0
+    //1 1 1
+    public static class CheckParity
+    {
+        public static bool IsEven(int x)
+        {
+            //0 & 1 == 0
+            //Since the last bit in an even number is 0, this will return true with an even number
+
+            return ((x & 1) == 0);
+        }
+        public static bool IsOdd(int x)
+        {
+            // 1 & 1 == 1
+            //Since the last bit in an odd number is 1, this will return true with an odd number
+            return ((x & 1) == 1);
+        }
+    }
+}
