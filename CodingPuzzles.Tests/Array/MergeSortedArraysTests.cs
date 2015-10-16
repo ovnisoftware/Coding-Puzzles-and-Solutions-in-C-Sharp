0fd613ca1bd11b6dfccf055a4c4d9b76c343a04c
ovnisoftware <info@ovnisoftware.com>
2015-10-16 11:59:37 -0400

    MergeSortedArrays.cs
    You are given two sorted arrays, A and B, where A has a large enough
    buffer at the end to hold B.  Write a method to merge B into A in sorted
    order.

---
+++ CodingPuzzles.Tests/Array/MergeSortedArraysTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.Array
+{
+    [TestClass]
+    public class MergeSortedArraysTests
+    {
+        [TestMethod]
+        public void MergeArrays_PassTwoSortedArrays_ReturnsSortedArray()
+        {
+            int[] x = new int[] { 1, 3, 5, 0, 0, 0, 0 };
+            int[] y = new int[] { 2, 4, 6, 7 };
+            int[] result = MergeSortedArrays.MergeArrays(x, y, 2);
+            int[] correct = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            CollectionAssert.AreEqual(correct, result);
+        }
+        [TestMethod]
+        public void MergeArrays_PassTwoEmptyArray_ReturnsSortedArray()
+        {
+            int[] x = new int[] { 1, 3, 5 };
+            int[] y = new int[] { };
+            int[] result = MergeSortedArrays.MergeArrays(x, y, 2);
+            int[] correct = new int[] { 1, 3, 5 };
+            CollectionAssert.AreEqual(correct, result);
+        }
+    }
+}
---
+++ CodingPuzzles/Array/MergeSortedArrays.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    //You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B.  Write a method to merge B into A in sorted order.
+    //Source: Cracking the Coding Interview 5th Edition p. 121
+    public static class MergeSortedArrays
+    {
+        //x array is assumed to be the length of x + y, and lastX is the position of the last stored element in x array
+        public static int[] MergeArrays(int[] x, int[] y, int lastX)
+        {
+            int xIndex = lastX;
+            int yIndex = y.Length - 1;
+            int mergeIndex = x.Length - 1;
+            while (yIndex >= 0)
+            {
+                if (y[yIndex] > x[xIndex])
+                {
+                    x[mergeIndex] = y[yIndex];
+                    yIndex--;
+                }
+                else if (y[yIndex] < x[xIndex])
+                {
+                    x[mergeIndex] = x[xIndex];
+                    xIndex--;
+                }
+                mergeIndex--;
+            }
+            return x;
+        }
+    }
+}
