a232c0b30069383343c4aeae431d1d3894b201eb
ovnisoftware <info@ovnisoftware.com>
2015-10-25 14:59:29 -0400

    PasswordChecker.cs
    Write password checker method - must contain min 6 char and max 12 char,
    No two similar chars consecutively, 1 lower case, 1 upper case, 1
    special char, no white space

---
+++ CodingPuzzles.Tests/StringManipulation/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.StringManipulation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckPassword_TooLong_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordChecker.CheckPassword("asdfasdfasdfasdfasdfasdf"));
+        }
+        [TestMethod]
+        public void CheckPassword_TooShort_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordChecker.CheckPassword("asdf"));
+        }
+        [TestMethod]
+        public void CheckPassword_NoUpperCase_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordChecker.CheckPassword("asdfasdf"));
+        }
+        [TestMethod]
+        public void CheckPassword_ContainsWhiteSpace_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordChecker.CheckPassword("as aasdf"));
+        }
+        [TestMethod]
+        public void CheckPassword_NoLowerCase_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordChecker.CheckPassword("ASDFASDF"));
+        }
+        [TestMethod]
+        public void CheckPassword_SameConsecutive_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordChecker.CheckPassword("AASaASDF"));
+        }
+        [TestMethod]
+        public void CheckPassword_NoSpecialCharacters_ReturnsFalse()
+        {
+            Assert.IsFalse(PasswordChecker.CheckPassword("aASaASDF"));
+        }
+        [TestMethod]
+        public void CheckPassword_GoodPassword_ReturnsTrue()
+        {
+            Assert.IsTrue(PasswordChecker.CheckPassword("aBcdef12&"));
+        }
+    }
+}
---
+++ CodingPuzzles/StringManipulation/PasswordChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    //Write password checker method - must contain min 6 char and max 12 char,
+    //No two similar chars consecutively, 1 lower case, 1 upper case, 1 special char, no white space 
+    public static class PasswordChecker
+    {
+        public static bool CheckPassword(string pass)
+        {
+            //min 6 chars, max 12 chars
+            if (pass.Length < 6 || pass.Length > 12)
+                return false;
+
+            //No white space
+            if (pass.Contains(" "))
+                return false;
+
+            //At least 1 upper case letter
+            if (!pass.Any(char.IsUpper))
+                return false;
+
+            //At least 1 lower case letter
+            if (!pass.Any(char.IsLower))
+                return false;
+
+            //No two similar chars consecutively
+            for (int i = 0; i < pass.Length - 1; i++)
+            {
+                if (pass[i] == pass[i + 1])
+                    return false;
+            }
+
+            //At least 1 special char
+            string specialCharacters = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
+            char[] specialCharactersArray = specialCharacters.ToCharArray();
+            foreach (char c in specialCharactersArray)
+            {
+                if (pass.Contains(c))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
