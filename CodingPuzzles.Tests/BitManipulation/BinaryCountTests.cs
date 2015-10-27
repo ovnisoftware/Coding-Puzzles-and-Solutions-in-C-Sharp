8d98a20f9dc0d35ef07a47e10bb5781ca3b581d4
ovnisoftware <info@ovnisoftware.com>
2015-10-27 12:06:38 -0400

    BinaryCount.cs
    Count binary strings with no consecutive ones

---
+++ CodingPuzzles.Tests/BitManipulation/BinaryCountTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.BitManipulation
+{
+    [TestClass]
+    public class BinaryCountTests
+    {
+        [TestMethod]
+        public void NumNoConsecutiveOnes_Passed3_Returns5()
+        {
+            Assert.AreEqual(5, BinaryCount.NumNoConsecutiveOnes(3));
+        }
+        [TestMethod]
+        public void NumNoConsecutiveOnes_Passed1_Returns2()
+        {
+            Assert.AreEqual(2, BinaryCount.NumNoConsecutiveOnes(1));
+        }
+        [TestMethod]
+        public void NumNoConsecutiveOnes_PassedNumberLessThan1_ThrowsException()
+        {
+            Exception exception = null;
+            try
+            {
+                BinaryCount.NumNoConsecutiveOnes(0);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+    }
+}
---
+++ CodingPuzzles/BitManipulation/BinaryCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    //Count binary strings with no consecutive ones 
+    //Source: https://www.youtube.com/watch?v=Bq9lgqC1YwE
+    public static class BinaryCount
+    {
+        public static int NumNoConsecutiveOnes(int x)
+        {
+            if (x < 1)
+                throw new Exception("Can not process a length of less than 1");
+            if (x == 1)
+                return 2;
+            if (x == 2)
+                return 3;
+            return NumNoConsecutiveOnes(x - 1) + NumNoConsecutiveOnes(x - 2);
+        }
+    }
+}
