a327b34e6e6a5a247ca01ef981a1139996ee3920
ovnisoftware <info@ovnisoftware.com>
2015-10-28 12:52:37 -0400

    ReverseInteger.cs
    Reverse Integer

---
+++ CodingPuzzles.Tests/Math/ReverseIntegerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.Math
+{
+    [TestClass]
+    public class ReverseIntegerTests
+    {
+        [TestMethod]
+        public void Reverse_Passed0_Returns0()
+        {
+            Assert.AreEqual(0, ReverseInteger.Reverse(0));
+        }
+        [TestMethod]
+        public void Reverse_Passed10_Returns1()
+        {
+            Assert.AreEqual(1, ReverseInteger.Reverse(10));
+        }
+
+        [TestMethod]
+        public void Reverse_Passed123_Returns321()
+        {
+            Assert.AreEqual(321, ReverseInteger.Reverse(123));
+        }
+        [TestMethod]
+        public void Reverse_PassedNegative123_ReturnsNegative321()
+        {
+            Assert.AreEqual(-321, ReverseInteger.Reverse(-123));
+        }
+        [TestMethod]
+        public void Reverse_Passed2147483647_ThrowsException()
+        {
+            Exception exception = null;
+            try
+            {
+                ReverseInteger.Reverse(2147483647);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+        [TestMethod]
+        public void Reverse_PassedNegative2147483648_ThrowsException()
+        {
+            Exception exception = null;
+            try
+            {
+                ReverseInteger.Reverse(-2147483648);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+    }
+}
---
+++ CodingPuzzles/Math/ReverseInteger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    //Reverse Integer
+    //Source: https://leetcode.com/problems/reverse-integer/
+    public static class ReverseInteger
+    {
+        public static int Reverse(int x)
+        {
+            if (x == 0)
+                return 0;
+
+            bool IsNegative = false;
+            if (x < 0)
+            {
+                IsNegative = true;
+                x = x * -1;
+            }
+
+            string result = "";
+            while (x > 0)
+            {
+                int digit = x % 10;
+                result += digit;
+                x = x / 10;
+            }
+
+            //Catches StackOverFlow Exception if x reversed is larger than 2,147,483,647 or less than –2,147,483,648
+            try
+            {
+                if (IsNegative)
+                    return (Convert.ToInt32(result) * -1);
+                else
+                    return Convert.ToInt32(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
