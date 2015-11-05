cc916b3bce224cec6bc05d2d15779afcecefac1f
ovnisoftware <info@ovnisoftware.com>
2015-11-04 20:47:01 -0500

    Added Unit Test

---
+++ CodingPuzzles.Tests/StringManipulation/ReverseStringTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.StringManipulation
+{
+    [TestClass]
+    public class ReverseStringTests
+    {
+        [TestMethod]
+        public void Reverse_PassedEmptyString_ReturnsEmptyString()
+        {
+            Assert.AreEqual("", ReverseString.Reverse(""));
+        }
+        [TestMethod]
+        public void Reverse_Passedabc_Returnscba()
+        {
+            Assert.AreEqual("cba", ReverseString.Reverse("abc"));
+        }
+    }
+}
