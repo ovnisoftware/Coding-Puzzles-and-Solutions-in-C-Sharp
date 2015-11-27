71e53cde907644a039132d91bb4c7af368e82c08
ovnisoftware <info@ovnisoftware.com>
2015-11-26 19:07:49 -0500

    BinaryToDecimal Unit Test

---
+++ CodingPuzzles.Tests/Math/Converter/BinaryToDecimalTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.Math.Converter
+{
+    [TestClass]
+    public class BinaryToDecimalTests
+    {
+        [TestMethod]
+        public void Convert_Pass1000_Returns8()
+        {
+            Assert.AreEqual(8, BinaryToDecimal.Convert("1000"));
+        }
+
+        [TestMethod]
+        public void Convert_Pass10111101_Returns189()
+        {
+            Assert.AreEqual(189, BinaryToDecimal.Convert("10111101"));
+        }
+
+        [TestMethod]
+        public void Convert_PassEmptyString_ThrowsException()
+        {
+            Exception exception = null;
+            try
+            {
+                BinaryToDecimal.Convert("");
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+        [TestMethod]
+        public void Convert_PassLetters_ThrowsException()
+        {
+            Exception exception = null;
+            try
+            {
+                BinaryToDecimal.Convert("abc");
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+        [TestMethod]
+        public void Convert_Pass3782_ThrowsException()
+        {
+            Exception exception = null;
+            try
+            {
+                BinaryToDecimal.Convert("3782");
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+    }
+}
--- CodingPuzzles/Math/Converter/BinaryToDecimal.cs
+++ CodingPuzzles/Math/Converter/BinaryToDecimal.cs
@@ -9,24 +9,35 @@ namespace CodingPuzzles
     public static class BinaryToDecimal
     {
         //Convert a binary string to an integer without using the Convert.ToInt32 class
-        public static int ConvertIt(string s)
+        public static int Convert(string s)
         {
+            if (s == "")
+                throw new Exception("Invalid input");
+
             int OnOff = 0;
             int result = 0;
 
             //Loop through each character in the passed string
             for (int i = 0; i < s.Length; i++)
             {
-                //Parse each char of the passed string
-                OnOff = Int32.Parse(s[i].ToString());
-                //If the char is a 1, add 2^(the position it's in) to the result
-                if (OnOff == 1)
-                    result += (int)Math.Pow(2, s.Length - 1 - i);
+                try
+                {
+                    //Parse each char of the passed string
+                    OnOff = Int32.Parse(s[i].ToString());
+
+                    //If the char is a 1, add 2^(the position it's in) to the result
+                    if (OnOff == 1)
+                        result += (int)Math.Pow(2, s.Length - 1 - i);
+
+                    else if (OnOff > 1)
+                        throw new Exception("Invalid input");
+                }
+                catch
+                {
+                    throw new Exception("Invalid input");
+                }
             }
             return result;
         }
-        ////Call in Program.cs to test
-        //Console.WriteLine(BinaryToDecimal.Convert("10111101")); 
-        ////Output should be 189
     }
 }
