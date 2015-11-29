6905ac5e8e01c91b287441d2a02172f2053af1cc
ovnisoftware <info@ovnisoftware.com>
2015-11-28 20:09:30 -0500

    Added unit test

---
+++ CodingPuzzles.Tests/Math/Converter/HexToBinaryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.Math.Converter
+{
+    [TestClass]
+    public class HexToBinaryTests
+    {
+        [TestMethod]
+        public void ConvertIt_Passabc123_returnsHex()
+        {
+            Assert.AreEqual("1010 1011 1100 0001 0010 0011 ", HexToBinary.ConvertIt("abc123"));
+        }
+        [TestMethod]
+        public void ConvertIt1_Passabc123_returnsHex()
+        {
+            Assert.AreEqual("1010 1011 1100 0001 0010 0011 ", HexToBinary.ConvertIt1("abc123"));
+        }
+    }
+}
