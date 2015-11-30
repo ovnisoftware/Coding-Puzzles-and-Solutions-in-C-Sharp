f19efb68ab886a7e3bf3973cbb37594a12364a31
ovnisoftware <info@ovnisoftware.com>
2015-11-29 20:00:21 -0500

    Added unit test

---
+++ CodingPuzzles.Tests/Math/Matrix/TicTacToeWinnerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests.Math.Matrix
+{
+    [TestClass]
+    public class TicTacToeWinnerTests
+    {
+        [TestMethod]
+        public void IsWinner_PassWinningBoard_ReturnsTrue()
+        {
+            int[,] a = new int[3, 3];
+            a[0, 0] = 0;
+            a[0, 1] = 0;
+            a[0, 2] = 0;
+            a[1, 0] = 1;
+            a[1, 1] = 1;
+            a[1, 2] = 1;
+            a[2, 0] = 0;
+            a[2, 1] = 0;
+            a[2, 2] = 0;
+            Assert.IsTrue(TicTacToeWinner.IsWinner(a));
+        }
+        [TestMethod]
+        public void IsWinner_PassInvalidInput_ThrowsException()
+        {
+            Exception exception = null;
+            try
+            {
+                int[,] a = new int[3, 3];
+                a[0, 0] = 0;
+                a[0, 1] = 0;
+                a[0, 2] = 0;
+                a[1, 0] = 5; //Invalid value
+                a[1, 1] = 1;
+                a[1, 2] = 1;
+                a[2, 0] = 0;
+                a[2, 1] = 0;
+                a[2, 2] = 0;
+                Assert.IsTrue(TicTacToeWinner.IsWinner(a));
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+    }
+}
--- CodingPuzzles/Math/Matrix/TicTacToeWinner.cs
+++ CodingPuzzles/Math/Matrix/TicTacToeWinner.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 namespace CodingPuzzles
 {
     //Write a function to determine if a TicTacToe board (3x3 board implemented as a 2D array) has a winner
-    //See code at bottom for example of a 3x3 board implemented with a 2D array
     public class TicTacToeWinner
     {
         public static bool IsWinner(int[,] x)
         {
             bool result = false;
+            for (int i = 0; i < x.GetUpperBound(0); i++)
+            {
+                for (int k = 0; k < x.GetUpperBound(0); k++)
+                {
+                    if ((x[i, k] != 0) && (x[i, k] != 1))
+                        throw new Exception("Invalid board, values must be only 1 or 0");
+                }
+            }
+
 
             //Checks for 3 x's straight across (3 cases)
             for (int i = 0; i < x.GetUpperBound(0); i++)
@@ -64,17 +72,4 @@ namespace CodingPuzzles
             return result;
         }
     }
-    ////Call this in Program.cs to test
-    //int[,] a = new int[3,3];
-    //a[0,0] = 0;
-    //a[0,1] = 0;
-    //a[0,2] = 0;
-    //a[1,0] = 1;
-    //a[1,1] = 1;
-    //a[1,2] = 1;
-    //a[2,0] = 0;
-    //a[2,1] = 0;
-    //a[2,2] = 0;
-    //Console.WriteLine(TicTacToeWinner.IsWinner(a));
-    //Should return true
 }
