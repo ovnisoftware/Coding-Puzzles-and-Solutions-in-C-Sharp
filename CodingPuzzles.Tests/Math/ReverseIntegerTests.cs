99f12e14b1b5943abe11cfcc1b5321f55e954f12
ovnisoftware <info@ovnisoftware.com>
2015-12-14 21:29:57 -0500

    Added unit test

--- CodingPuzzles.Tests/Math/ReverseIntegerTests.cs
+++ CodingPuzzles.Tests/Math/ReverseIntegerTests.cs
@@ -16,6 +16,11 @@ namespace CodingPuzzles.Tests.Math
         {
             Assert.AreEqual(1, ReverseInteger.Reverse(10));
         }
+        [TestMethod]
+        public void Reverse_Passed100_Returns1()
+        {
+            Assert.AreEqual(1, ReverseInteger.Reverse(100));
+        }
 
         [TestMethod]
         public void Reverse_Passed123_Returns321()
