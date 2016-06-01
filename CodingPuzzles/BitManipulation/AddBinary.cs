b8132d7cee183817caf0127256f897642f90240d
ovnisoftware <info@ovnisoftware.com>
2016-06-01 07:50:57 -0400

    AddBinary.cs
    Given two binary strings, return their sum (also a binary string).

---
+++ CodingPuzzles/BitManipulation/AddBinary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    public static class AddBinary
+    {
+        public static string Sum(string x, string y)
+        {
+            //convert x to int
+            int num1 = Convert.ToInt32(x, 2);
+
+            //convert y to int
+            int num2 = Convert.ToInt32(y, 2);
+
+            //add them
+            int result = num1 + num2;
+
+            //convert result to binary
+            return Convert.ToString(result, 2);
+        }
+    }
+}
